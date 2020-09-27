694a96a04e5224bc15ce1d3900efe41c124aedfe
YouGotMyBluePixel <49995801+YouGotMyBluePixel@users.noreply.github.com>
2020-09-26 22:05:15 -0400

    update

--- js/main.js
+++ js/main.js
@@ -60,4 +60,22 @@ window.onload = function() {
 };
 
 
-Resources
+//Handle Menu
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    });
+});
+
+
+
+
+
